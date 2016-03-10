8e0508b6b1253c2c54bad1fa9b0dc1f299a50e78
Tyler Rusch <ruschta@uwec.edu>
2016-03-10 12:28:59 -0600

    Initial commit

---
+++ 3D Racer/Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+    public float distanceAbove;
+    public float distanceBehind;
+    private float accumRotation;
+
+    private float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.Q))
+        {
+            accumRotation++;
+        } else if (Input.GetKey(KeyCode.E))
+        {
+            accumRotation--;
+        }
+        transform.position = target.position - (target.forward * distanceBehind) + (Vector3.up * distanceAbove * (Input.mousePosition.y * 0.1f));
+        transform.LookAt(target);
+        transform.RotateAround(target.transform.position, Vector3.up, Input.mousePosition.x * 0.5f);
+	}
+}
