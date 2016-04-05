fe9f57f48985cd5661f9eb5b215d23a937a32118
Tyler Rusch <ruschta@uwec.edu>
2016-04-05 09:13:54 -0500

    committing KATAMARI

---
+++ 3D Racer/Assets/katamari.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class katamari : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            this.transform.SetParent(other.transform);
+            this.GetComponent<Collider>().isTrigger = false;
+            this.transform.tag = "Player";
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            this.transform.SetParent(other.transform);
+            //this.GetComponent<Collider>().isTrigger = false;
+            this.transform.tag = "Player";
+        }
+    }
+}
